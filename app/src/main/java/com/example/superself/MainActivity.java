dc95c7446ba5b819e8f9c118377cac12810f7b47
Deon Zoss <deonquentinzoss@gmail.com>
2019-02-19 09:22:02 -0600

    superself added ui elements

---
+++ app/src/main/java/com/example/superself/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.superself;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void dayMaker (View view){
+
+        Intent intent = new Intent(this, DayMakerActivity.class);
+        startActivity(intent);
+
+    }
+}
